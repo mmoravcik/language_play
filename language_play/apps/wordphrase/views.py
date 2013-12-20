447f7c6caff830632a4531a29e0b2709669b6c85
Matus Moravcik <matus.moravcik@gmail.com>
2013-12-20 18:30:44 +0000

    Create detail view + random word view

--- language_play/apps/wordphrase/views.py
+++ language_play/apps/wordphrase/views.py
@@ -1,5 +1,11 @@
-from language_play.apps.wordphrase.forms import SettingsForm
 from django.views.generic.edit import FormView
+from django.views.generic.detail import DetailView
+from django.views.generic.base import RedirectView
+from django.core.urlresolvers import reverse
+from django.utils.text import slugify
+
+from language_play.apps.wordphrase.forms import SettingsForm
+from language_play.apps.wordphrase.models import WordPhrase
 
 
 class SettingsFormView(FormView):
@@ -15,7 +21,33 @@ class SettingsFormView(FormView):
         self.request.session['show_wordphrase'] = form.cleaned_data['show_wordphrase']
         self.request.session['show_translations'] = form.cleaned_data['show_translations']
 
-
-
-
         return super(SettingsFormView, self).form_valid(form)
+
+
+class WordPhraseView(DetailView):
+    model = WordPhrase
+
+    def get_object(self, queryset=None):
+        return self.model.objects.get(pk=self.kwargs['wordphrase_id'])
+
+    def get_context_data(self, **kwargs):
+        ctx = super(WordPhraseView, self).get_context_data()
+        translations = self.object.translations.filter(language=self.request.session['destination_lang'])
+
+        ctx['translations'] = translations
+        ctx['show_images'] = self.request.session.get('show_images', True)
+        ctx['show_wordphrase'] = self.request.session.get('show_wordphrase', True)
+        ctx['show_translations'] = self.request.session.get('show_translations', True)
+
+        return ctx
+
+
+class GetRandomWordPhraseView(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        wordphrase = WordPhrase.objects.filter(language=self.request.session['source_lang']).order_by('?')[0]
+        params = dict(
+            language_code=wordphrase.language.code,
+            wordphrase_id=wordphrase.pk,
+            wordphrase_text=slugify(wordphrase.text),
+        )
+        return reverse('wordphrase-detail', kwargs=params)
--- language_play/urls.py
+++ language_play/urls.py
@@ -6,15 +6,24 @@ from django.views.generic import TemplateView
 
 admin.autodiscover()
 
-from views import HomeView
 from language_play.apps.wordphrase import views
 
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', HomeView.as_view(), name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^alphabet/slovak/$', TemplateView.as_view(template_name='alphabet/slovak.html'), name='alphabet-slovak'),
-    url(r'^language/select/$', views.SettingsFormView.as_view(), name='language-select'),
+    url(r'^$',
+        TemplateView.as_view(template_name='home.html'),
+        name='home'),
+    url(r'^wordphrase/(?P<language_code>\w+)/(?P<wordphrase_id>\d+)/(?P<wordphrase_text>)[\w\-]+/$',
+        views.WordPhraseView.as_view(),
+        name='wordphrase-detail'),
+    url(r'^wordphrase/random/$',
+        views.GetRandomWordPhraseView.as_view(),
+        name='wordphrase-random'),
+    url(r'^alphabet/slovak/$',
+        TemplateView.as_view(template_name='alphabet/slovak.html'),
+        name='alphabet-slovak'),
+    url(r'^language/select/$',
+        views.SettingsFormView.as_view(),
+        name='language-select'),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
