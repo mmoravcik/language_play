1c76166838d5f135f78c11611e0e121e925e5660
Matus Moravcik <matus.moravcik@gmail.com>
2013-12-21 22:17:43 +0000

    Fix media root setting

--- language_play/conf/default.py
+++ language_play/conf/default.py
@@ -76,7 +76,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'public/media'),
+MEDIA_ROOT = os.path.join(BASE_DIR, 'public/media')
 MEDIA_URL = '/media/'
 
 # Database
