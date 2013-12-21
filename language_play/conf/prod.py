a2fd50cb385ed7c6dba671a557e389a797c89fea
Matus Moravcik <matus.moravcik@gmail.com>
2013-12-21 22:45:39 +0000

    Prod config

--- language_play/conf/prod.py
+++ language_play/conf/prod.py
@@ -1,1 +1,14 @@
 DEBUG = False
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'language_play_jess',                      # Or path to database file if using sqlite3.
+        'USER': 'language_jess',                      # Not used with sqlite3.
+        'PASSWORD': 'the_dummy_password',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
